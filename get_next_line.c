38f650ba4ab1eac17d5749c549ffc07ce31c6f99
thribeir <thiagoribeirochico@gmail.com>
2025-09-12 18:11:59 +0100

    edge cases on read stash

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: thribeir <thribeir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/08 11:58:38 by thribeir          #+#    #+#             */
-/*   Updated: 2025/09/11 17:55:19 by thribeir         ###   ########.fr       */
+/*   Updated: 2025/09/12 18:11:15 by thribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ char	*read_to_stash(fd, stash)
 	size_t	nread;
 	char	buf[BUFFER_SIZE + 1];
 
-	while (gnl_strchr(slash, '\n'))
+	while (!gnl_strchr(stash, '\n'))
 	{
 		nread = read(fd, buf, BUFFER_SIZE);
+		if (nread == 0)
+			return (stash);
+		if (nread == -1)
+			return (NULL);
 		buf[nread] = '\0';
 		stash = gnl_strjoin(stash, buf);
 	}
